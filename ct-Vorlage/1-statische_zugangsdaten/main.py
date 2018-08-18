80789435ba9ae1449ce7d575155dd4d4cc3761ef
Tobias Haegele <haegelix@yahoo.de>
2018-08-18 15:49:50 +0200

    Initialer Commit

---
+++ ct-Vorlage/1-statische_zugangsdaten/main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class WLAN(BoxLayout):
+    pass
+
+class GuestWLANApp(App):
+    pass
+
+if __name__ == "__main__":
+    GuestWLANApp().run()
